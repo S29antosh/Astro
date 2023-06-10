c16b24269383cac200e5da73278b3c5695b44718
Santosh Yada <ymanoutube@gmail.com>
2023-06-10 14:17:30 +0545

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-//   site: "https://S29antosh.github.io",
-//   base: "/my-repo",
+  site: "https://S29antosh.github.io",
+  base: "/Astro",
 });
